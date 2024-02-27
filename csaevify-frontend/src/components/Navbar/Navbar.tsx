2727f910440abfaf532eb3e8015928cd76cb4552
krisztianavi <avikrisz@gmail.com>
2024-02-27 18:02:05 +0100

    Created the main logo

--- csaevify-frontend/src/components/Navbar/Navbar.tsx
+++ csaevify-frontend/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import "./Navbar.css";
+import csaevify from '../../images/csaevifywhite.svg';
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-first">
-        <h1>Csaevify</h1>
+        <img className="csaevifyicon" src={csaevify} alt="cseavifyicon" />
       </div>
       <div className="navbar-second">
         <Link to={"/discover"} className="link">
