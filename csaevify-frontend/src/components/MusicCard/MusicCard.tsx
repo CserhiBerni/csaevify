c07a1ea491c39586e4027ccb18a83ae9ac772908
krisztianavi <avikrisz@gmail.com>
2024-02-27 15:48:31 +0100

    Updated MusicCard

--- csaevify-frontend/src/components/MusicCard/MusicCard.tsx
+++ csaevify-frontend/src/components/MusicCard/MusicCard.tsx
@@ -12,9 +12,14 @@ const MusicCard = () => {
   const [play, setPlay] = useState(false);
   const [loop, setLoop] = useState(false);
   const [currentVolume, setCurrentVolume] = useState(MAX);
+  const [showVolumeControls, setShowVolumeControls] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
   const kakaRef = useRef<HTMLAudioElement | null>(null);
+
+  const toggleVolumeControls = () => {
+    setShowVolumeControls(!showVolumeControls);
+  };
   
 
   function toggleAudio(): void {
@@ -101,32 +106,26 @@ const MusicCard = () => {
         <div className="play" onClick={toggleAudio}>
           {play ? <FaPause size={20} /> : <FaPlay size={20} />}
         </div>
-        <div className="volumebar-container">
-          <div className="volumebar-q">
-            <div className="vb">
-              <input
-                type="range"
-                className="volumebar"
-                min={0}
-                max={MAX}
-                onChange={handleVolume}
-                style={{ background: `linear-gradient(90deg, purple 0%, orange ${(currentVolume / MAX) * 100}%, white ${(currentVolume / MAX) * 100}%, white 100%)` }}
-              />
-              <FaVolumeUp size={20}/>
+            <div className="volumebar-container">
+              <div className="volumebar-q">
+                    <div className="vb">
+                        {showVolumeControls && (<input type="range" className="volumebar" min={0} max={MAX} onChange={handleVolume}
+                         style={{ background: `linear-gradient(90deg, purple 0%, orange ${(currentVolume / MAX) * 100}%, white ${(currentVolume / MAX) * 100}%, white 100%)` }}/>)}
+                        <FaVolumeUp size={20} className="volumeicon" onClick={toggleVolumeControls} style={{color: "grey"}} />
+                    </div>
+                  {showVolumeControls && <p className="currentVolume">{currentVolume}</p>}
+              </div>
             </div>
-            <p className="currentVolume">{currentVolume}</p>
-          </div>
-        </div>
         <div className="buttons-container">
           <div className="buttons">
             <div className="shuffle audio-btn">
               <FaShuffle size={20} color={"#D1E8E2"} />
             </div>
             <div className="loop audio-btn" onClick={toggleLoop}>
-              <TiArrowLoop size={30} color={loop ? "orange" : "white"} />
+              <TiArrowLoop size={30} style={{color: "white", background: loop ? "linear-gradient(90deg, purple, orange)" : "none", borderRadius: 15}} />
             </div>
             <div className="like audio-btn">
-              <FaHeart size={20} color={"#19747E"} />
+              <FaHeart size={20} color={"linear-gradient(90deg, purple, orange)"} />
             </div>
           </div>
         </div>
