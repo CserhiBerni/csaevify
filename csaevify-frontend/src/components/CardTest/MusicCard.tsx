7651670ab663b0ec9b16462d0db846aeb288e704
CserhiBerni <59263479+CserhiBerni@users.noreply.github.com>
2024-03-11 12:21:02 +0100

    final commit.

--- csaevify-frontend/src/components/CardTest/MusicCard.tsx
+++ csaevify-frontend/src/components/CardTest/MusicCard.tsx
@@ -1,13 +1,25 @@
 import "./MusicCard.css";
-import cover from '../../images/vultures.jpeg'
-import audio from '../../audios/burn.mp3'
+import vultures from "../../images/vultures.jpeg";
+import institution from "../../images/institution.png";
+import gyerekdalok from "../../images/gyerekdalok.png";
+import burn from "../../audios/burn.mp3";
+import already from '../../audios/already.mp3';
+import fejesvallak from '../../audios/fejesvallak.mp3';
+
 import { FaPlay, FaPause, FaHeart, FaVolumeUp } from "react-icons/fa";
 import { FaShuffle } from "react-icons/fa6";
 import { TiArrowLoop } from "react-icons/ti";
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from "react";
 
+interface Song {
+  release: string;
+  cover: string;
+  title: string;
+  artist: string;
+  audio: string;
+}
 
-const MusicCard = () => {
+const MusicCard = (props: Song) => {
   const MAX = 100;
 
   const [play, setPlay] = useState(false);
@@ -83,81 +95,105 @@ const MusicCard = () => {
     const seconds = Math.floor(time % 60);
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   }
-
   return (
     <>
-    <div className="music-card-container">
-      <div className="music-card">
-        <p className="release">2024 · VULTURES 1</p>
-        <img src={cover} alt="cover" className="cover" />
-        <p className="title">Burn</p>
-        <p className="artist">¥$, Kanye West, Ty Dolla $ign</p>
-        <div className="slider">
-          <input
-            type="range"
-            className="slider"
-            value={currentTime}
-            max={duration || 0}
-            onChange={handleSliderChange}
+      <div className="music-card-container">
+        <div className="music-card">
+          <p className="release">{props.release}</p>
+          <img
+            src={
+              props.cover == "institution"
+                ? institution
+                : props.cover == "vultures"
+                ? vultures
+                : gyerekdalok
+            }
+            alt="cover"
+            className="cover"
           />
-          <p className="counter">
-            {formatTime(currentTime)} / {formatTime(duration)}
-          </p>
-        </div>
-        <div className="play" onClick={toggleAudio}>
-          {play ? <FaPause size={20} /> : <FaPlay size={20} />}
-        </div>
-        <div className="volumebar-container">
-          <div className="volumebar-q">
-            <div className="vb">
-              {showVolumeControls && (
-                <input
-                  type="range"
-                  className="volumebar"
-                  min={0}
-                  max={MAX}
-                  onChange={handleVolume}
-                  style={{
-                    background: `linear-gradient(90deg, purple 0%, orange ${
-                      (currentVolume / MAX) * 100
-                    }%, white ${(currentVolume / MAX) * 100}%, white 100%)`,
-                  }}
+          <p className="title">{props.title}</p>
+          <p className="artist">{props.artist}</p>
+          <div className="slider">
+            <input
+              type="range"
+              className="slider"
+              value={currentTime}
+              max={duration || 0}
+              onChange={handleSliderChange}
+            />
+            <p className="counter">
+              {formatTime(currentTime)} / {formatTime(duration)}
+            </p>
+          </div>
+          <div className="play" onClick={toggleAudio}>
+            {play ? <FaPause size={20} /> : <FaPlay size={20} />}
+          </div>
+          <div className="volumebar-container">
+            <div className="volumebar-q">
+              <div className="vb">
+                {showVolumeControls && (
+                  <input
+                    type="range"
+                    className="volumebar"
+                    min={0}
+                    max={MAX}
+                    onChange={handleVolume}
+                    style={{
+                      background: `linear-gradient(90deg, purple 0%, orange ${
+                        (currentVolume / MAX) * 100
+                      }%, white ${(currentVolume / MAX) * 100}%, white 100%)`,
+                    }}
+                  />
+                )}
+                <FaVolumeUp
+                  size={20}
+                  className="volumeicon"
+                  onClick={toggleVolumeControls}
+                  style={{ color: "grey" }}
                 />
+              </div>
+              {showVolumeControls && (
+                <p className="currentVolume">{currentVolume}</p>
               )}
-              <FaVolumeUp
-                size={20}
-                className="volumeicon"
-                onClick={toggleVolumeControls}
-                style={{ color: "grey" }}
+            </div>
+          </div>
+          <div className="buttons">
+            <div className="shuffle audio-btn">
+              <FaShuffle size={20} color={"#D1E8E2"} />
+            </div>
+            <div className="loop audio-btn" onClick={toggleLoop}>
+              <TiArrowLoop
+                size={30}
+                style={{
+                  color: "white",
+                  background: loop
+                    ? "linear-gradient(90deg, purple, orange)"
+                    : "none",
+                  borderRadius: 15,
+                }}
               />
             </div>
-            {showVolumeControls && (
-              <p className="currentVolume">{currentVolume}</p>
-            )}
+            <div className="like audio-btn">
+              <svg width="20" height="20">
+                <defs>
+                  <linearGradient
+                    id="gradient"
+                    x1="0%"
+                    y1="0%"
+                    x2="100%"
+                    y2="0%"
+                  >
+                    <stop offset="0%" stopColor="#800080" />
+                    <stop offset="100%" stopColor="#FFA500" />
+                  </linearGradient>
+                </defs>
+                <FaHeart size={20} fill="url(#gradient)" />
+              </svg>
+            </div>
           </div>
         </div>
-        <div className="buttons">
-          <div className="shuffle audio-btn">
-            <FaShuffle size={20} color={"#D1E8E2"} />
-          </div>
-          <div className="loop audio-btn" onClick={toggleLoop}>
-            <TiArrowLoop size={30} style={{color: "white", background: loop ? "linear-gradient(90deg, purple, orange)" : "none", borderRadius: 15}} />
-          </div>
-          <div className="like audio-btn">
-            <svg width="20" height="20">
-              <defs>
-                <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-                  <stop offset="0%" stopColor="#800080" />
-                  <stop offset="100%" stopColor="#FFA500" />
-                </linearGradient>
-              </defs>
-              <FaHeart size={20} fill="url(#gradient)"/>
-            </svg>
-          </div>
-        </div>
+        <audio ref={musicref} src={props.audio == "already" ? already : props.audio == "burn" ? burn : fejesvallak} />
       </div>
-      <audio ref={musicref} src={audio} />
-    </div>
     </>
   );
 };
--- csaevify-frontend/src/pages/Landing/LandingPage.tsx
+++ csaevify-frontend/src/pages/Landing/LandingPage.tsx
@@ -1,12 +1,21 @@
 import "./LandingPage.css";
 import Navbar from "../../components/Navbar/Navbar";
 import MusicCard from "../../components/CardTest/MusicCard";
-import vultures from '../../images/vultures.jpeg'
-import institution from '../../images/institution.png'
-import gyerekdalok from '../../images/gyerekdalok.png'
+import vultures from "../../images/vultures.jpeg";
+import institution from "../../images/institution.png";
+import gyerekdalok from "../../images/gyerekdalok.png";
 import { FaPlay } from "react-icons/fa";
+import { useState } from "react";
 
 const LandingPage = () => {
+  const [currentMusic, setCurrentMusic] = useState({
+    release: "Vultures - 2024",
+    cover: "vultures",
+    title: "Burn",
+    artist: "¥$, Kanye West, Ty Dolla $ign",
+    audio: "burn",
+  });
+
   return (
     <div className="landing-page">
       <Navbar />
@@ -18,7 +27,18 @@ const LandingPage = () => {
               <p className="song-title">Burn</p>
               <p>¥$, Kanye West, Ty Dolla $ign</p>
               <div className="song-play">
-                <FaPlay className="song-play-icon"/>
+                <FaPlay
+                  className="song-play-icon"
+                  onClick={() => {
+                    setCurrentMusic({
+                      release: "Vultures - 2024",
+                      cover: "vultures",
+                      title: "Burn",
+                      artist: "¥$, Kanye West, Ty Dolla $ign",
+                      audio: "burn",
+                    });
+                  }}
+                />
               </div>
             </div>
             <div className="song">
@@ -26,7 +46,18 @@ const LandingPage = () => {
               <p className="song-title">Already</p>
               <p>Kodak Black</p>
               <div className="song-play">
-                <FaPlay className="song-play-icon"/>
+                <FaPlay
+                  className="song-play-icon"
+                  onClick={() => {
+                    setCurrentMusic({
+                      release: "Institution - 2015",
+                      cover: "institution",
+                      title: "Already",
+                      artist: "Kodak Black",
+                      audio: "already",
+                    });
+                  }}
+                />
               </div>
             </div>
             <div className="song">
@@ -34,12 +65,29 @@ const LandingPage = () => {
               <p className="song-title">Fej és vállak</p>
               <p>HeyKids Gyerekdalok</p>
               <div className="song-play">
-                <FaPlay className="song-play-icon"/>
+                <FaPlay
+                  className="song-play-icon"
+                  onClick={() => {
+                    setCurrentMusic({
+                      release: "Gyerekdalok - 2017",
+                      cover: "gyerekdalok",
+                      title: "Fej és Vállak",
+                      artist: "HeyKids Gyerekdalok",
+                      audio: "fejesvallak",
+                    });
+                  }}
+                />
               </div>
             </div>
           </div>
         </div>
-        <MusicCard />
+        <MusicCard
+          release={currentMusic.release}
+          artist={currentMusic.artist}
+          cover={currentMusic.cover}
+          title={currentMusic.title}
+          audio={currentMusic.audio}
+        />
       </div>
     </div>
   );
